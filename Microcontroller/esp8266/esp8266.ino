8167b66f7d3d699590437fd169657e57a8d07b23
jensmoths <61699505+jensmoths@users.noreply.github.com>
2020-04-09 12:54:14 +0200

    Jens (#6)
    * Create esp8266.ino
    
    Med kod för enkel wifiupppkoppling
    
    * La till location parameter i wifi-konfigurationen

--- Microcontroller/esp8266/esp8266.ino
+++ Microcontroller/esp8266/esp8266.ino
@@ -7,20 +7,25 @@
 
 
 
-void setupWifiManager(){
-WiFiManager wifiManager;
-  
+
+void setupWifiManager() {
+  WiFiManager wifiManager;
+
   //uncomment to reset saved settings
   //wifiManager.resetSettings();
+
+  //Parameter for configuring the location at the same time as wifi
+  WiFiManagerParameter location("location", "location", "", 40);
+  wifiManager.addParameter(&location);
 
   //fetches ssid and pass from eeprom and tries to connect
   //if it does not connect it starts an access point with the specified name
   //and goes into a BLOCKING loop awaiting configuration
   wifiManager.autoConnect("ConnectToSetupSecuritySystem");
-  
+
   //if you get here you have connected to the WiFi
   Serial.println("connected to wifi");
-  
+  Serial.println(location.getValue());
 }
 
 

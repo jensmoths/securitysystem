fdb32332ae45f183affc67f08b522fa6a62c4918
P4CIFIC <malek_123_chawki@hotmail.com>
2020-05-04 00:46:56 +0200

    Added logging capability and sending/showing to global client

--- securitysystem/src/globalClient/GlobalClientController.java
+++ securitysystem/src/globalClient/GlobalClientController.java
@@ -1,15 +1,13 @@
 package globalClient;
 
+import globalServer.Logger;
+
 public class GlobalClientController {
     private GlobalClient globalClient;
     private MainFrame mainFrame;
 
     public GlobalClientController () {
-
-     
-
         globalClient = new GlobalClient("localhost",8081, this);
-
         mainFrame = new MainFrame(globalClient);
     }
 
@@ -18,6 +16,9 @@ public class GlobalClientController {
         mainFrame.showMainPanel();
     }
 
+    public void updateLog(Logger logger) {
+        mainFrame.setTextArea(logger.getLog(23, 47, 23, 49));
+    }
 
     public static void main(String[] args) {
         new GlobalClientController();
--- securitysystem/src/globalServer/RequestHandler.java
+++ securitysystem/src/globalServer/RequestHandler.java
@@ -7,33 +7,48 @@ import java.io.IOException;
 public class RequestHandler {
 
     private EmailSender emailSender = new EmailSender();
+    private Home home;
+
+    public RequestHandler(Home home) {
+        this.home = home;
+    }
 
     public void handleServerRequest(Object requestObject, Home home) {
 
         if (requestObject instanceof String) {
+            home.logger.addToLogger((String) requestObject);
+            home.sendToAllClients(home.logger);
             home.sendToAllClients(requestObject);
         } else if (requestObject instanceof Message) {
             Message message = (Message) requestObject;
             SecurityComponent securityComponent = message.getSecurityComponent();
 
             if (securityComponent instanceof MagneticSensor) {
-                System.out.println("You are in magnet sensor");
-                System.out.println(securityComponent.isOpen());
 
                 if (securityComponent.isOpen()) {
                     home.sendToAllClients("Magnetsensorn larmar");
+                    home.logger.addToLogger("Magnetsensorn larmar");
+                    home.sendToAllClients(home.logger);
 
                 } else if (!securityComponent.isOpen()) {
                     home.sendToAllClients("Magnetsensorn är aktiv");
+                    home.logger.addToLogger("Magnetsensorn är aktiv");
+                    home.sendToAllClients(home.logger);
+
                 }
             }
             if (securityComponent instanceof FireAlarm) {
-                System.out.println("You are in Firealarm");
                 home.sendToAllClients("Brandlarmet har upptäckt rök i byggnaden");
+                home.logger.addToLogger("Brandlarmet har upptäckt rök i byggnaden");
+                home.sendToAllClients(home.logger);
+
             }
+
             if (securityComponent instanceof ProximitySensor) {
-                System.out.println("You are in Proximity Sensor");
                 home.sendToAllClients("Rörelsedetektorn har upptäckt rörelse i byggnaden");
+                home.logger.addToLogger("Rörelsedetektorn har upptäckt rörelse i byggnaden");
+                home.sendToAllClients(home.logger);
+
             }
         }
     }
@@ -50,9 +65,13 @@ public class RequestHandler {
                 break;
             case "lock":
                 messageResponse = new Message("", new DoorLock(false));
+                home.logger.addToLogger("Door locked");
+                home.sendToAllClients(home.logger);
                 break;
             case "unlock":
                 messageResponse = new Message("", new DoorLock(true));
+                home.logger.addToLogger("Door unlocked");
+                home.sendToAllClients(home.logger);
                 break;
         }
 
